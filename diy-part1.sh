3f1738a5c8e460e77f43392cc93f1de5c843d8df
ronggq <71620489+ronggq@users.noreply.github.com>
2022-07-09 11:00:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,16 +7,20 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=============================================================
-
 # fw876/helloworld
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.defaultault
 
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-
-#sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
-#sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
+#sed -i '$a  src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+
+# lua-maxminddb
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+
+# luci-app-vssr
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
+
 #rm -rf target/linux/ramips
 #svn export https://github.com/padavanonly/immortalwrt/trunk/target/linux/ramips target/linux/ramips
